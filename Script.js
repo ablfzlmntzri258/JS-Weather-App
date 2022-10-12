abf1ca5f21a547b47a7bae9c8b61adcacdd86bda
Abolfazl Montazeri <abolfazle.m82@gmail.com>
2022-10-12 17:50:32 +0330

    Test commit with Webstorm

--- Script.js
+++ Script.js
@@ -1,2 +1,17 @@
+const insideDiv = document.querySelectorAll(".heading,.p1,.p2,.input,.get")
+const loaderElements = document.querySelectorAll(".lds-hourglass,.p3")
+
+
+function loader() {
+    for (var i = 0; i < insideDiv.length; i++) {
+        insideDiv[i].style.display = "none"
+    }
+    loaderElements[0].style.display = "inline-block"
+    loaderElements[1].style.display = "block"
+
+}
+
+document.querySelector(".get").addEventListener("click",loader)
+
 
 
